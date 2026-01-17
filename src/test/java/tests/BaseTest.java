bf3a5bc3fdb055470c7b9fa33bdfe2d1d8b11cdb
QA academy <education@qaacademy.rs>
2026-01-17 10:45:45 +0100

    BaseTest 15th line

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,6 +4,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
 
@@ -11,7 +12,7 @@ protected static WebDriver driver;
 
     @BeforeClass
     public static void beforeClass() throws Exception {
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.get("https://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
     }
